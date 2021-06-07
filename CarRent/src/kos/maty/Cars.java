869468a615f7521a071ca4bbf976bae8982c5db6
Akosmaty <83218806+Akosmaty@users.noreply.github.com>
2021-06-07 13:43:37 +0200

    Delete CarRent directory

--- CarRent/src/kos/maty/Cars.java
+++
@@ -1,9 +0,0 @@
-package kos.maty;
-
-public interface Cars {
-    public String getBrand();
-    public int getEngine();
-    public String getColor();
-    public String getModel();
-    public String toString();
-}
